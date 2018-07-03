33ef70fb7e9c85cf338c93ba425f147cd73747ca
Johan Hermansson <oan@oans.net>
2018-07-03 20:13:08 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const fs                    = require( 'fs' );
+const path                  = require( 'path' );
+const MiniCssExtractPlugin  = require( 'mini-css-extract-plugin' );
+const UglifyJSPlugin        = require( 'uglifyjs-webpack-plugin' );
+const SVGSpriteMapPlugin    = require( 'svg-spritemap-webpack-plugin' );
+const WebpackAssetsManifest = require( 'webpack-assets-manifest' );
+const BuildNotifierPlugin   = require( 'webpack-build-notifier' );
+
+const siteName     = 'Webpack 4'; // Used in build notifier
+const env          = process.env.NODE_ENV;
+const prod         = 'production' === env;
+const themeDir     = '.'; // Or dist directory in your WordPress theme
+const distDir      = themeDir + '/dist';
+const manifestPath = path.resolve( __dirname, distDir + '/manifest.json' );
+const oldManifest  = fs.existsSync( manifestPath ) ? JSON.parse( fs.readFileSync( manifestPath, 'utf8' ) ) : {};
+
+module.exports = {
+	context: __dirname,
+	entry: {
+		app: path.resolve( __dirname, 'src/js/app' ),
+	},
+	output: {
+		path: path.resolve( __dirname, distDir ),
+		filename: '[name]-[hash].js',
+		chunkFilename: '[id]-[chunkhash].js',
+	},
+	plugins: [
+		new MiniCssExtractPlugin( {
+			filename: '[name]-[hash].css',
+			chunkFilename: '[id]-[hash].css',
+		} ),
+		new SVGSpriteMapPlugin( {
+			src: 'src/svg/**/*.svg',
+			prefix: '',
+			filename: 'sprite-[contenthash].svg'
+		} ),
+		new WebpackAssetsManifest( {
+			// Remove old manifest files if they are changed
+			done( obj, stats ) {
+				const newManifest = obj.assets;
+				Object.keys( oldManifest ).forEach( key => {
+					const oldFile = path.resolve( __dirname, distDir + '/' + oldManifest[ key ] );
+					if ( newManifest[ key ] !== oldManifest[ key ] && fs.existsSync( oldFile ) ) {
+						fs.unlink( oldFile, () => {} );
+					}
+				} );
+			}
+		} ),
+		new BuildNotifierPlugin( {
+		    title: siteName,
+			successSound: false,
+		} ),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+	  				loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							importLoader: 2,
+							sourceMap: ! prod,
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: ! prod,
+							syntax: 'postcss-scss',
+							plugins: loader => [
+						      require( 'postcss-import' )( {
+								  root: loader.resourcePath,
+							  } ),
+						      require( 'postcss-preset-env' )( {
+								  stage: 3,
+							  } ),
+						      require( 'autoprefixer' )(),
+						      require( 'cssnano' )( {
+								  autoprefixer: false
+							  } ),
+							  require( 'postcss-flexbugs-fixes' )(),
+						  ],
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: ! prod,
+						},
+					}
+				],
+			},
+		],
+	},
+};
